93d8cb558f4ccc1bf607edd842d38de633b7bf41
apaple <76417012+apaple@users.noreply.github.com>
2022-12-07 16:29:52 +0700

    Create babi.sh

---
+++ babi.sh
@@ -0,0 +1,21 @@
+sudo apt install npm -y
+sudo apt install screen -y
+npm i chalk
+npm i request
+npm i random-ua
+npm i sync-request
+npm add sync-request
+npm i cluster
+npm i fs
+npm i http2
+npm i tls
+npm i superagent
+npm i superagent-proxy
+npm i axios
+npm i zombie
+npm i cloudscraper
+npm add prompt-sync
+chmod u+x *
+ulimit 9999
+sudo screen
+sudo node bot
