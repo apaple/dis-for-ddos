77a75d613fcb1e3a9a8cd37d28667a5258ad8b81
apaple <76417012+apaple@users.noreply.github.com>
2023-10-04 20:48:56 +0700

    Update babi.sh

--- babi.sh
+++ babi.sh
@@ -1,4 +1,10 @@
 yum install epel-release -y
+sudo add-apt-repository universe -y
+apt install python3 -y
+apt install python3-pip -y
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+source ~/.profile
+nvm install v16.0.0
 yum install wget -y
 yum install npm -y
 yum install nodejs -y
@@ -7,6 +13,8 @@ yum install python -y
 yum install python3 -y
 pip install -r requirements.txt
 pip3 install pysocks
+pip3 install httpx
+pip3 install bs4
 npm i chalk
 npm i request
 npm i libquery
