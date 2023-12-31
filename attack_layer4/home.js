999f613850cfe9ca17341ed0c70124a81f69e72d
apaple <76417012+apaple@users.noreply.github.com>
2023-12-31 14:09:16 +0700

    Update home.js

--- attack_layer4/home.js
+++ attack_layer4/home.js
@@ -25,7 +25,7 @@ if(!args[0]) {
 
 // Command attack
 var exec = require('child_process').exaec
-exec(`python3 main.py && rm -f proxy.txt`, (error, stdout, stderr) => {
+exec(`python3 main.py`, (error, stdout, stderr) => {
 });
 
 // Start Attacking
